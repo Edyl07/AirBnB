0850ad7decb70cd69305b7c4084c99ad9e6d5c5b
Demba <demba94diagana@gmail.com>
2021-08-14 21:06:44 +0000

    init & Home Screen

---
+++ src/screens/Home/Index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { ImageBackground, Pressable, Text, View } from 'react-native'
+import Fontisto from 'react-native-vector-icons/Fontisto'
+import styles from './styles'
+
+const HomeScreen = (props) => {
+    return (
+        <View>
+
+            <Pressable
+                style={styles.searchButton}
+                onPress={() => navigation.navigate('Destination Search')}>
+                <Fontisto name="search" size={25} color={'#f15454'} />
+                <Text style={styles.searchButtonText}>Where are you going?</Text>
+            </Pressable>
+
+            <ImageBackground
+                source={require('../../../assets/images/wallpaper.jpg')}
+                style={styles.image}>
+                <Text style={styles.title}>Go Near</Text>
+
+                <Pressable
+                    style={styles.button}
+                    onPress={() => console.warn('Explore Btn clicked')}>
+                    <Text style={styles.buttonText}>Explore nearby stays</Text>
+                </Pressable>
+            </ImageBackground>
+        </View>
+    )
+}
+
+
+
+export default HomeScreen;
+
